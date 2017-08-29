780d50c5b113df465398be43f605d7d5468e2f1a
Macbook <macbook@erics-mbp.mchenry.edu>
2017-08-29 11:02:08 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello MAD_105");
+	System.out.println("My Name is Eric Witowski");
+	System.out.println("I am a motivational speaker and I lover programming everyday");
+	System.out.println("I have little experience in programming and work 48 hours each week on my dream");
+	System.out.println("I will give my 120% best because this is what I want to do the next 30 years");
+	System.out.println("I am not only doing this for myself, but also doing it for a better life for my kids");
+    }
+}
